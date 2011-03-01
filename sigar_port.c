0d77579a78baac504485b5f73ac4c735db5ab1a3
Aliaksey Kandratsenka <alk@tut.by>
2011-02-28 17:21:48 -0800

    added copyright header and reformatted for membase C coding style

--- sigar_port.c
+++ sigar_port.c
@@ -1,3 +1,19 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2011 Membase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -34,10 +50,12 @@ int main(void)
 	while (!feof(stdin)) {
 		int req;
 		int rv = fread(&req, sizeof(req), 1, stdin);
-		if (rv < 1)
+		if (rv < 1) {
 			continue;
-		if (req != 0)
+        }
+		if (req != 0) {
 			break;
+        }
 		memset(&reply, 0, sizeof(reply));
 		reply.version = 0;
 		reply.struct_size = sizeof(reply);
